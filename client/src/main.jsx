a5be4ba9d3e5a54feae6c0363f593885c4e14f82
alasiri <austinalasiri44@gmail.com>
2025-07-16 14:19:19 +0300

    added client template

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {RouterProvider , createBrowserRouter} from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { routes } from './router.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter(routes)
+
+
+const root = createRoot(document.getElementById('root'))
+root.render(
+  < RouterProvider router={router} />
 )
+
