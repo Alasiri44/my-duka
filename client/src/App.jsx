34ead0f914d6424261a2de19542d3b365301d28e
Mercy <maritimmercy8@gmail.com>
2025-07-16 23:37:09 +0300

    added app.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import SupplyRequestTable from './components/admin/SupplyRequestTable';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🛒 MyDuka Admin Dashboard</h1>
+      <SupplyRequestTable />
+    </div>
+  );
+}
+
+
+export default App;
