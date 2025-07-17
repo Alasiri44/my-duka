0fc1134750816e20f7f1619de00a580f14b30788
alasiri <austinalasiri44@gmail.com>
2025-07-17 23:22:21 +0300

    corrected errors on main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {RouterProvider , createBrowserRouter} from 'react-router-dom'
 import './index.css'
+import App from './App.jsx'
+import { routes } from './router.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter(routes)
+
+const root = createRoot(document.getElementById('root'))
+root.render(
+  < RouterProvider router={router} />
 )
+
