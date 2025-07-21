3fc3bcd30852daa13a3a602690c556c25c22af3c
Mercy <maritimmercy8@gmail.com>
2025-07-21 15:47:14 +0300

    added requests table

--- client/src/pages/dashboards/admin/clerks.jsx
+++ client/src/pages/dashboards/admin/clerks.jsx
@@ -1,11 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  fetchClerks,
-  deactivateClerk,
-  deleteClerk,
-  addClerk
-} from '../../store/clerksSlice';
+import { fetchClerks, deactivateClerk, deleteClerk , addClerk} from '../../store/clerksSlice';
 import axios from 'axios';
 
 const Clerks = () => {
