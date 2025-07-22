e552ee9d0b4ffb03ef7344e873c451555b5d860d
Carlos Korir <carlkorir226@gmail.com>
2025-07-22 04:57:44 +0300

    fixes the rotes to product details

--- client/src/routes/clerk.tsx
+++ client/src/routes/clerk.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { RouteObject } from "react-router-dom";
 import ClerkLayout from "../components/clerk/index";
 import ClerkDash from "../pages/dashboards/clerk/ClerkDash";
-import Inventory from "../pages/dashboards/clerk/Inventory"; // renamed for clarity
+import Inventory from "../pages/dashboards/clerk/Inventory"; 
 import CategoryPage from "../pages/dashboards/clerk/CategoryPage";
 import ProductDetail from "../pages/dashboards/clerk/ProductDetail";
 import AddProduct from "../pages/dashboards/clerk/AddProduct";
