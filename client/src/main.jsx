bd871eb10b3c8d6bf8546e89cffc6a9bf01022e7
alasiri <austinalasiri44@gmail.com>
2025-07-22 12:24:49 +0300

    changes to main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,16 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import store from './store/index.js'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { routes } from "./router.jsx";
-
-const router = createBrowserRouter(routes)
+import { store } from './redux/store';
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  < RouterProvider router={router}/>
-  // <Provider store={store}>
-  //   <App />
-  // </Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
