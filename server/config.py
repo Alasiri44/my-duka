28e3b834f73991230ccce36f1f15220abdf83370
Carlos Korir <carlkorir226@gmail.com>
2025-07-25 07:46:10 +0300

    adds migrations

--- server/config.py
+++ server/config.py
@@ -1,2 +1,2 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:P057gr3s@localhost:5432/my_duka_db'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:carlos1234@localhost:5432/my_duka_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
