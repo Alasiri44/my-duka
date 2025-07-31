352aba34b720b191286d47dbf1b312c7d98eaebe
alasiri <austinalasiri44@gmail.com>
2025-07-31 22:00:52 +0300

    allowing seeding

--- server/app.py
+++ server/app.py
@@ -20,7 +20,7 @@ from controllers.batch_controller import batch_bp
 from controllers.sale_controller import sale_bp
 from controllers.mpesa_controller import mpesa_bp
 from controllers.reports_controller import reports_bp
-# from scripts.seed_db import run_seed
+from scripts.seed_db import run_seed
 
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
@@ -28,7 +28,7 @@ import os
 
 app = create_app()
 bcrypt = Bcrypt(app)
-CORS(app, supports_credentials=True, origins=["https://myduka-xylv.onrender.com"])
+CORS(app, supports_credentials=True, origins=["https://my-duka-c8kn.onrender.com"])
 
 app.register_blueprint(merchant_bp)
 app.register_blueprint(business_bp)
@@ -50,8 +50,8 @@ app.register_blueprint(mpesa_bp)
 
 # Serve React frontend
 @app.route('/')
-def serve_vue(path):  
-    return make_response('<h1>Welcome to my duka</h1>', 200)
+def home():
+    return make_response('<h1>Welcome to this Flask app</h1>')
 
 
 @app.route('/api/mpesa/callback', methods=['POST'])
@@ -64,6 +64,68 @@ def mpesa_callback():
 
     return make_response(({"ResultCode": 0, "ResultDesc": "Callback received successfully"}), 200)
 
+@app.route('/run-seed')
+def seed_route():
+    try:
+        with current_app.app_context():
+            run_seed()
+        return {"message": "Database seeded successfully."}, 200
+    except Exception as e:
+        return {"error": str(e)}, 500
+
+@app.route("/reset-user-id-sequence")
+def reset_user_id_sequence():
+    try:
+        db.session.execute("""
+            SELECT setval('users_id_seq', (SELECT MAX(id) FROM "users"));
+        """)
+        db.session.execute("""
+            SELECT setval('merchants_id_seq', (SELECT MAX(id) FROM "merchants"));
+        """)
+        db.session.execute("""
+            SELECT setval('batches_id_seq', (SELECT MAX(id) FROM "batches"));
+        """)
+        db.session.execute("""
+            SELECT setval('business_settings_id_seq', (SELECT MAX(id) FROM "business_settings"));
+        """)
+        db.session.execute("""
+            SELECT setval('businesses_id_seq', (SELECT MAX(id) FROM "businesses"));
+        """)
+        db.session.execute("""
+            SELECT setval('categories_id_seq', (SELECT MAX(id) FROM "categories"));
+        """)
+        db.session.execute("""
+            SELECT setval('invites_id_seq', (SELECT MAX(id) FROM "invites"));
+        """)
+        db.session.execute("""
+            SELECT setval('payments_id_seq', (SELECT MAX(id) FROM "payments"));
+        """)
+        db.session.execute("""
+            SELECT setval('products_id_seq', (SELECT MAX(id) FROM "products"));
+        """)
+        db.session.execute("""
+            SELECT setval('sales_id_seq', (SELECT MAX(id) FROM "sales"));
+        """)
+        db.session.execute("""
+            SELECT setval('stock_entries_id_seq', (SELECT MAX(id) FROM "stock_entries"));
+        """)
+        db.session.execute("""
+            SELECT setval('stock_exits_id_seq', (SELECT MAX(id) FROM "stock_exits"));
+        """)
+        db.session.execute("""
+            SELECT setval('stores_id_seq', (SELECT MAX(id) FROM "stores"));
+        """)
+        db.session.execute("""
+            SELECT setval('suppliers_id_seq', (SELECT MAX(id) FROM "suppliers"));
+        """)
+        db.session.execute("""
+            SELECT setval('supply_requests_id_seq', (SELECT MAX(id) FROM "supply_requests"));
+        """)
+        db.session.commit()
+        return {"message": "id_seq updated successfully"}, 200
+    except Exception as e:
+        db.session.rollback()
+        return {"error": str(e)}, 500
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
